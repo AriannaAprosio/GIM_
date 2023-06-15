21c52e88a3d4df27a6953400b0b9ab0217a1764e
AriannaAprosio <131662919+AriannaAprosio@users.noreply.github.com>
2023-06-15 17:33:16 +0200

    modifica

--- Esercizio_4/2_orologio_digitale/sketch.js
+++ Esercizio_4/2_orologio_digitale/sketch.js
@@ -1,23 +1,46 @@
-function setup(){
-	createCanvas(windowWidth, windowHeight)
+var x;
+var y;
+var r;
+var g;
+var b;
+
+function setup() {
+  createCanvas(1000, 1000);
+  
+  ellipseMode(CORNERS);
+  angleMode(DEGREES); 
+  
+  r =random(0, 255);
+  g =random(0, 255);
+  b =random(0, 255);
+
+  hour_angle = 0;
+  minute_angle = 0;
 }
 
-function draw(){
-	//background(220,220,220)
+function draw() {
+  background(0, 100);
+  
+  r = r + random(-5, 5);
+  g = g + random(-5, 5);
+  b = b + random(-5, 5);
+  
+  r= constrain(r, 0, 255);
+  g= constrain(g, 0, 255);
+  b= constrain(b, 0, 255);
+  
+ 
 
-	let ora = hour() + ":" + minute() + ":" + second()
-    
-	
+  draw_flower(width/2,height/2, 300, second(), 70, 60, 6);
+}
 
-
-	textSize(40)
-	textFont("monospace")
-	textAlign(CENTER, CENTER)
-	fill((255), random (255), random(255))
-
-	text(ora, mouseX, mouseY)
-
-
-
-
+function draw_flower(x_translate, y_translate, leaf_radius, num_of_leaves, elipse_width, elipse_length, rotate_angle){
+  push();
+  translate(x_translate, y_translate);
+  for(var i=0; i < num_of_leaves; i++) {
+    fill(r, g, b);
+    rotate(rotate_angle);
+    ellipse(leaf_radius, 0, elipse_width, elipse_length);
+ }
+  pop();
 }
